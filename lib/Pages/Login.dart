5b09f30efadba12979f769c3d40fd96fa42b2294
solomon-winner <solyalew3@gmail.com>
2024-07-06 23:24:44 +0300

    ...

--- lib/Pages/Login.dart
+++ lib/Pages/Login.dart
@@ -23,9 +23,62 @@ class _LoginState extends State<Login> {
   
   Widget _buildUI() {
     return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        
+        _title(),
+        _loginForm(),
       ],
+    );
+  }
+
+  Widget _title() {
+    return const Text(
+      "Recipe Book",
+      style: TextStyle(
+        fontSize: 35,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+
+  }
+  
+  Widget _loginForm() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.90,
+      height: MediaQuery.sizeOf(context).height * 0.30,
+      child: Form(child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "User Name",
+            ),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "PassWord",
+            ),
+          ),
+          _LoginButton(),
+        ],
+      ),
+      ),
+    );
+  }
+
+  Widget _LoginButton () {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.60,
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text(
+         "Login",
+         ),
+      ), 
     );
   }
 }
