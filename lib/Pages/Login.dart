02e7b32b3cbe885aef6a2a6c1672bfd36a15f5c4
solomon-winner <solyalew3@gmail.com>
2024-07-06 21:57:22 +0300

    ...

---
+++ lib/Pages/Login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+  
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
