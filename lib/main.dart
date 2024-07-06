4a6eff0b69164fb566b893b527bf9c98e24da1f9
solomon-winner <solyalew3@gmail.com>
2024-07-06 21:51:48 +0300

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text("Hello Solomon!"),
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Recipe App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+        seedColor: Colors.deepOrange,
+        ),
+        useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-    ),
-  ),
-  );
+      home: ,
+    );
+  }
+  
 }
