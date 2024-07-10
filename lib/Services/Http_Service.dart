c0eb24c78db6fc77e7e76c843ed942384888b373
solomon-winner <solyalew3@gmail.com>
2024-07-10 15:42:02 +0300

    ...

--- lib/Services/Http_Service.dart
+++ lib/Services/Http_Service.dart
@@ -44,4 +44,14 @@ setup();
       print(e);  
     }
   }
+
+  Future<Response?> get(String path) async {
+    try {
+      final response = await _dio.get(path);
+      return response;
+    } catch (e) {
+      print(e);
+    }
+    return null;
+  }
 }
