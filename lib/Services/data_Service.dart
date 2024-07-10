a2f8e98b755e40c147c0b33a850df5589c432f9d
solomon-winner <solyalew3@gmail.com>
2024-07-10 16:21:24 +0300

    ...

--- lib/Services/data_Service.dart
+++ lib/Services/data_Service.dart
@@ -15,7 +15,20 @@ class DataService {
     var response = await _httpService.get(path);
 
     if (response?.statusCode == 200 && response?.data != null) {
-      print(response!.data);
+      List data = response!.data["recipes"];
+      print("Response data: $data");
+           try {
+        List<Recipe> recipes = data.map((e) {
+          print("Parsing recipe: $e");
+          return Recipe.fromJson(e);
+        }).toList();
+        print("Parsed recipes: $recipes");
+        return recipes;
+      } catch (e) {
+        print("Error parsing recipes: $e");
+        return null;
+      }
+     // return recipes;
     }
   }
 }
