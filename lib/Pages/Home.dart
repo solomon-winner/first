344219880ae287ffedf7dd1e5eca27cf418d1ef4
solomon-winner <solyalew3@gmail.com>
2024-07-11 13:21:51 +0300

    ...

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -10,6 +10,9 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+String mealTypeFilter = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
