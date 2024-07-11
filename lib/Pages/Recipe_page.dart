c6b92dfb7197046d0d845cc97c1e4541722148f9
solomon-winner <solyalew3@gmail.com>
2024-07-11 14:16:43 +0300

    ...

---
+++ lib/Pages/Recipe_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class RecipePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white60,
+        title: const Text(
+          "RecipeBook",
+        ),
+      ),
+      body: _buildUI(),
+    );
+      
+  }
+Widget _buildUI() {
+        return Column(
+          children: [],
+        );
+      }
+    
+}
